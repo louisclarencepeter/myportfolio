0d5c666ca20265b1e2552d15945550cec86ed946
Louis Peter <louisclarencepeters@gmail.com>
2023-09-24 16:36:49 +0200

    created a main component which has Hero, About and Projects components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Main />
         </div>
     );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <>
+            <Hero />
+            <About />
+            <Projects />
+        </>
+    )
+}
+
+export default Main
