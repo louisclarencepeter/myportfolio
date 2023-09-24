decd9ab62dced7167c2115a42a50c73d2fece3cf
Louis Peter <louisclarencepeters@gmail.com>
2023-09-24 17:57:52 +0200

    removed all the code and style for the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,18 @@
-import React from 'react'
-import logo from '../../assets/images/me.jpg'
-import './Header.scss'
+import React, { useState } from 'react';
+import logo from '../../assets/images/me.jpg';
+import './Header.scss';
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(prevMenuOpen => !prevMenuOpen);
+    };
+
     return (
-        < >
-            <nav>
-                <div class="navbar">
-                    <div class="container nav-container">
-                        <input class="checkbox" type="checkbox" name="" id="" />
-                        <div class="hamburger-lines">
-                            <span class="line line1"></span>
-                            <span class="line line2"></span>
-                            <span class="line line3"></span>
-                        </div>
-                        <div class="logo">
-                            <img src={logo} alt="logo" srcset="" />
-                        </div>
-                        <div class="menu-items">
-                            <li><a href="#">About Me</a></li>
-                            <li><a href="#">My Projects</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+        <>
         </>
-    )
+    );
 }
 
-export default Header
+export default Header;
