d5ef6c70bc3e7ea348ede19587159f825e326a64
Louis Peter <louisclarencepeters@gmail.com>
2023-09-27 14:45:30 +0200

    added style to the navbar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,41 +1,55 @@
-import React from 'react'
-import './Navbar.scss'
-import logo from '../../assets/images/me.jpg'
+import React, { useEffect } from 'react';
+import './Navbar.scss';
+import logo from '../../assets/images/me.jpg';
 
 function Navbar() {
+  useEffect(() => {
+    const navbar = document.querySelector('.navbar');
+
+    const handleScroll = () => {
+      if (window.pageYOffset >= navbar.offsetTop) {
+        navbar.classList.add('sticky');
+      } else {
+        navbar.classList.remove('sticky');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <nav>
-        <div className="navbar">
-          <div className="container nav-container">
-            <input className="checkbox" type="checkbox" name="" id="" />
-            <div className="hamburger-lines">
-              <span className="line line1" />
-              <span className="line line2" />
-              <span className="line line3" />
-            </div>
-            <div className="logo">
-              <img src={logo} alt="logo" />
-            </div>
-            <div className="menu-items">
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About Me</a>
-              </li>
-              <li>
-                <a href="#">My Projects</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
-            </div>
-          </div>
+    <nav className="navbar">
+      <div className="container nav-container">
+        <input className="checkbox" type="checkbox" name="" id="" />
+        <div className="hamburger-lines">
+          <span className="line line1" />
+          <span className="line line2" />
+          <span className="line line3" />
         </div>
-      </nav>
-    </>
-  )
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="menu-items">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Me</a>
+          </li>
+          <li>
+            <a href="#">My Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function About() {
   return (
-    <div></div>
+    <>
+    </>
   )
 }
 
