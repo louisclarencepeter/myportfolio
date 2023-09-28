4c1f60e15a49aaffd7f5b936bcb55aecd7480ed7
Louis Peter <louisclarencepeters@gmail.com>
2023-09-28 14:14:40 +0200

    added a backdrop filter for the class menu-items

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -2,12 +2,11 @@ import React, { useEffect } from 'react';
 import './Navbar.scss';
 import logo from '../../assets/images/me.jpg';
 
-function Navbar() {
+const Navbar = () => {
   useEffect(() => {
-    const navbar = document.querySelector('.navbar');
-
     const handleScroll = () => {
-      if (window.pageYOffset >= navbar.offsetTop) {
+      const navbar = document.querySelector('.navbar');
+      if (navbar && window.pageYOffset >= navbar.offsetTop) {
         navbar.classList.add('sticky');
       } else {
         navbar.classList.remove('sticky');
@@ -24,28 +23,21 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="container nav-container">
-        <input className="checkbox" type="checkbox" name="" id="" />
+        <input className="checkbox" type="checkbox" />
         <div className="hamburger-lines">
-          <span className="line line1" />
-          <span className="line line2" />
-          <span className="line line3" />
+          {[1, 2, 3].map((line) => (
+            <span key={line} className={`line line${line}`} />
+          ))}
         </div>
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
         <div className="menu-items">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About Me</a>
-          </li>
-          <li>
-            <a href="#">My Projects</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
+          {['Home', 'About Me', 'My Projects', 'Contact'].map((item, index) => (
+            <li key={index}>
+              <a href="#">{item}</a>
+            </li>
+          ))}
         </div>
       </div>
     </nav>
