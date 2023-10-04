de02edd2e089abce5c7e462e7eaf4866f39482d7
Louis Peter <louisclarencepeters@gmail.com>
2023-10-04 16:03:57 +0200

    rewrote the code in SASS

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -1,64 +1,55 @@
-import React from 'react'
-import './About.scss'
+// About.js
+
+import React from 'react';
+import './About.scss';
 import logo from '../../assets/images/me.jpg';
 import dci from '../../assets/images/dci.svg';
 import skills from '../../assets/images/skills.jpg';
 
-function About() {
+const About = () => {
   return (
     <>
-      {/* Main Content */}
-  <main>
-    {/* My Picture and Small Info */}
-    <section>
-      <div className="info bounce-top one">
-        <img src={logo} alt="my picture" />
-        <h2>Louis Peter</h2>
-        <p>
-          Living in Frankfrurt am Main with passion of Technolgy and Web
-          Development
-        </p>
-        <a href="https://github.com/louisclarencepeter" target="_blank">
-          <i className="fa-brands fa-github" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/louisclarencepeter/"
-          target="_blank"
-        >
-          <i className="fa-brands fa-linkedin" />
-        </a>
-      </div>
-    </section>
-    {/* School*/}
-    <section>
-      <div className="info bounce-top two">
-        <img
-          src={dci}
-          alt="my school"
-        />
-        <h2>Education</h2>
-        <p>2022 - Present</p>
-        <p>Full Stack Web Development</p>
-        <p>Digital Career Institute</p>
-      </div>
-    </section>
-    {/* My Progress */}
-    <section>
-      <div className="info bounce-top three">
-        <img src={skills} alt="my Progress" />
-        <h2>My Skills</h2>
-        <p>HTML</p>
-        <p>CSS</p>
-        <p>JavaScript</p>
-        <p>Bootstrap</p>
-        <p>Git</p>
-        <p>React</p>
-        <p>Node.js</p>
-      </div>
-    </section>
-  </main>
-    </>
-  )
-}
+      <main className='main'>
+        <section className='about'>
+          <div className='info bounce-top one'>
+            <img src={logo} alt='my picture' />
+            <h2>Louis Peter</h2>
+            <p>Living in Frankfurt am Main with a passion for Technology and Web Development</p>
+            <a href='https://github.com/louisclarencepeter' target='_blank'>
+              <i className='fa-brands fa-github' />
+            </a>
+            <a href='https://www.linkedin.com/in/louisclarencepeter/' target='_blank'>
+              <i className='fa-brands fa-linkedin' />
+            </a>
+          </div>
+        </section>
 
-export default About
+        <section className='about'>
+          <div className='info bounce-top two'>
+            <img src={dci} alt='my school' />
+            <h2>Education</h2>
+            <p>2022 - Present</p>
+            <p>Full Stack Web Development</p>
+            <p>Digital Career Institute</p>
+          </div>
+        </section>
+
+        <section className='about'>
+          <div className='info bounce-top three'>
+            <img src={skills} alt='my Progress' />
+            <h2>My Skills</h2>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>Bootstrap</p>
+            <p>Git</p>
+            <p>React</p>
+            <p>Node.js</p>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default About;
