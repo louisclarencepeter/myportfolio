6cff2207c8ddd167cfc1b15393f6d18bf25dfde7
Louis Peter <louisclarencepeters@gmail.com>
2023-11-13 11:27:58 +0100

    fixed the input checkbox bug on devices higher than 1025px

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './Navbar.scss';
 import logo from '../../assets/images/me.jpg';
 
-const Navbar = () => {
-  const [isChecked, setIsChecked] = useState(false);
+const menuItems = ['Home', 'About Me', 'My Projects', 'Contact'];
 
+const useStickyNavbar = () => {
   useEffect(() => {
+    const navbar = document.querySelector('.navbar');
     const handleScroll = () => {
-      const navbar = document.querySelector('.navbar');
       if (window.pageYOffset >= navbar.offsetTop) {
         navbar.classList.add('sticky');
       } else {
@@ -16,36 +16,58 @@ const Navbar = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
+};
 
-  const handleMenuItemClick = () => {
-    // Uncheck the checkbox to close the menu
-    setIsChecked(false);
-  };
+const NavbarItem = ({ item, handleMenuItemClick }) => (
+  <li onClick={handleMenuItemClick}>
+    <a href={`#${item.replace(/\s+/g, '').toLowerCase()}`}>{item}</a>
+  </li>
+);
+
+const Navbar = () => {
+  const [isChecked, setIsChecked] = useState(false);
+  useStickyNavbar();
+
+  const handleMenuItemClick = useCallback(() => setIsChecked(false), []);
 
   return (
     <nav className="navbar">
       <div className="container nav-container">
-        <input className="checkbox" type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
-        <div className="hamburger-lines" onClick={() => setIsChecked(!isChecked)}>
-          {[1, 2, 3].map((line) => (
-            <span key={line} className={`line line${line}`} />
-          ))}
+
+        <div className="classicalmenu" >
+          <div className="classicalmenu-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <ul>
+            {menuItems.map((item, index) => (
+              <NavbarItem key={index} item={item} handleMenuItemClick={handleMenuItemClick} />
+            ))}
+          </ul>
         </div>
+
+        <input 
+          className="checkbox" 
+          type="checkbox" 
+          checked={isChecked} 
+          onChange={(e) => setIsChecked(e.target.checked)} 
+        />
+
+        <div className="hamburger-lines" onClick={() => setIsChecked(!isChecked)}>
+          {[1, 2, 3].map((line) => <span key={line} className={`line line${line}`} />)}
+        </div>
+
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
+
         <ul className="menu-items">
-          {['Home', 'About Me', 'My Projects', 'Contact'].map((item, index) => (
-            <li key={index} onClick={handleMenuItemClick}>
-              <a href={`#${item.replace(/\s+/g, '').toLowerCase()}`}>{item}</a>
-            </li>
+          {menuItems.map((item, index) => (
+            <NavbarItem key={index} item={item} handleMenuItemClick={handleMenuItemClick} />
           ))}
         </ul>
+
       </div>
     </nav>
   );
